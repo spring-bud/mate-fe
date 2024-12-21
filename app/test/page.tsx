aae66b5a23687699aa4373b5a132e83982181acd
clyde-yoonjae <leewh77777@gmail.com>
2024-12-22 02:11:52 +0900

    feat: footer 섹션 생성#2

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -3,15 +3,17 @@ import Banner from './components/Banner';
 import { PopularProduct } from './components/PopularProduct';
 import Category from './components/Category';
 import PopularUserProfile from './components/PopularUserProfile';
+import Footer from '@/components/shared/footer/Footer';
 
 export default function Home() {
   return (
-    <div className="container h-[3000px]">
+    <div className="container">
       <Header />
       <Banner />
       <PopularProduct />
       <Category />
       <PopularUserProfile />
+      <Footer />
     </div>
   );
 }
