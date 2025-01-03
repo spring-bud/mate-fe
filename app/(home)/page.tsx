dfc5caa1cd9461d37fe8f327509f9cf10d2d006e
clyde-yoonjae <leewh77777@gmail.com>
2025-01-03 17:10:27 +0900

    chore: 폴더이름변경에 의한 경로수정#6

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,7 @@
-import Banner from './components/Banner';
-import { PopularProduct } from './components/PopularProduct';
-import Category from './components/Category';
-import PopularUserProfile from './components/PopularUserProfile';
+import Banner from './sections/Banner';
+import { PopularProduct } from './sections/PopularProduct';
+import Category from './sections/Category';
+import PopularUserProfile from './sections/PopularUserProfile';
 
 export default function Home() {
   return (
