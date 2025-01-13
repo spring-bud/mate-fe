efc2b99f1c038cabd0b4adc68648ef6abbac5089
clyde-yoonjae <leewh77777@gmail.com>
2025-01-14 02:13:49 +0900

    chore: 이미지컴포넌트 오류핸들러 함수추가#12

--- utils/customImage/CustomImage.tsx
+++ utils/customImage/CustomImage.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import Image from 'next/image';
+import { useState } from 'react';
 
 interface Props {
   src: string;
@@ -10,7 +13,23 @@ interface Props {
 }
 
 const CustomImage = ({ src, alt, width, height, onClick, className }: Props) => {
-  return <Image className={className} src={src} alt={alt} width={width} height={height} onClick={onClick} />;
+  const [imageSrc, setImageSrc] = useState(src);
+
+  const handleError = () => {
+    setImageSrc('/images/mock/UserProfileSample1.png');
+  };
+
+  return (
+    <Image
+      className={className}
+      src={imageSrc}
+      alt={alt}
+      width={width}
+      height={height}
+      onClick={onClick}
+      onError={handleError}
+    />
+  );
 };
 
 export default CustomImage;
