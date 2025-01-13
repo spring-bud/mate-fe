608b59e7aeacefd807c2a8f98c08b1c9070d557b
clyde-yoonjae <leewh77777@gmail.com>
2025-01-14 02:25:58 +0900

    test: Mock데이터용 렌더링위한 config임시수정#12

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['via.placeholder.com'],
+    domains: ['example.com'],
   },
   webpack(config) {
     config.module.rules.push({
