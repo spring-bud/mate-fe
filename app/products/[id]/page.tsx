7838fc6f13800d5e042d8c159b352b2e226627e3
clyde-yoonjae <leewh77777@gmail.com>
2025-01-19 01:33:14 +0900

    fix: 빌드에러 수정 기존 parms타입 변경#12

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,24 +1,26 @@
-// ProductPage.tsx
 import { QueryClient, dehydrate, HydrationBoundary } from '@tanstack/react-query';
 import { productAPI } from '@/service/product/api';
 import { QUERY_KEYS } from '@/service/product/constants';
 import Ui from './ui/Ui';
 
-export default async function ProductPage({ params }: { params: { id: string } }) {
+type PageParams = Promise<{ id: string }>;
+
+export default async function ProductPage({ params }: { params: PageParams }) {
+  const { id } = await params;
   const queryClient = new QueryClient();
 
-  if (!params?.id) {
+  if (!id) {
     throw new Error('params.id is required but missing');
   }
 
   await Promise.all([
     queryClient.prefetchQuery({
-      queryKey: QUERY_KEYS.product.detail(params.id),
-      queryFn: () => productAPI.getProduct(params.id),
+      queryKey: QUERY_KEYS.product.detail(id),
+      queryFn: () => productAPI.getProduct(id),
     }),
     queryClient.prefetchQuery({
-      queryKey: QUERY_KEYS.product.reviews(params.id),
-      queryFn: () => productAPI.getReviews(params.id),
+      queryKey: QUERY_KEYS.product.reviews(id),
+      queryFn: () => productAPI.getReviews(id),
     }),
   ]);
 
@@ -26,7 +28,7 @@ export default async function ProductPage({ params }: { params: { id: string } }
 
   return (
     <HydrationBoundary state={dehydratedState}>
-      <Ui productId={params.id} />
+      <Ui productId={id} />
     </HydrationBoundary>
   );
 }
